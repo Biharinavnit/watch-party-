db7ea0566f4bdec762d91c8352912991aa0ee05b
biharinavnit <biharinavnit28@gmail.com>
2026-05-22 14:08:17 +0530

    connect deployed backend

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,7 @@
 import { io } from "socket.io-client";
 
-
-const socket = io("http://localhost:5000");
+const socket = io(
+  "https://watch-party-backend-rgcf.onrender.com"
+);
 
 export default socket;
-
-
